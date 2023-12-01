72a0f54674d0f350d34b964f42642fc9a529d502
jorgesoft <jorgecarrilloit@gmail.com>
2023-12-01 03:09:02 -0500

    created html report and uploaded it to aws s3

--- nmap/scan.sh
+++ nmap/scan.sh
@@ -16,11 +16,26 @@ TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
 
 # Running a quick nmap scan on the specified IP range and saving the reports
 echo "Performing a quick scan on the network: $IP_RANGE"
-#nmap -T4 -F --webxml -oA $REPORT_DIR/scan_$TIMESTAMP $IP_RANGE
-nmap -T4 -F $IP_RANGE
+nmap -T4 -F --webxml -oA $REPORT_DIR/scan_$TIMESTAMP $IP_RANGE
+#nmap -T4 -F $IP_RANGE
+
+# Creating html report from xml file
+xsltproc -o $REPORT_DIR/scan_$TIMESTAMP.html /usr/share/nmap/nmap.xsl $REPORT_DIR/scan_$TIMESTAMP.xml
+
 
 # Rename the webxml file to HTML
 #mv $REPORT_DIR/scan_$TIMESTAMP.webxml $REPORT_DIR/scan_$TIMESTAMP.html
 
 echo "Scan complete. Reports are stored in $REPORT_DIR"
+
+# S3 Bucket details
+BUCKET_NAME="jorges-test"
+FOLDER_NAME="scanning-results"
+
+# Upload the scan reports to the S3 bucket
+aws s3 cp $REPORT_DIR/scan_$TIMESTAMP.html s3://$BUCKET_NAME/$FOLDER_NAME/scan_$TIMESTAMP.html
+aws s3 cp $REPORT_DIR/scan_$TIMESTAMP.xml s3://$BUCKET_NAME/$FOLDER_NAME/scan_$TIMESTAMP.xml
+
+echo "Reports uploaded to S3 bucket: $BUCKET_NAME/$FOLDER_NAME"
+
 sleep infinity
