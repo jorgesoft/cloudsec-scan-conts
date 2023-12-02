2a27fc154cc44085c2745f02c23d7ef22d56f07a
jorgesoft <jorgecarrilloit@gmail.com>
2023-12-02 14:00:14 -0500

    initial prowler container working

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cloudsec-scan-conts
+
+## Build images:
+
+docker build -t ecs-nmap .
+docker tag ecs-nmap:latest gorje6/ecs-nmap:latest
+docker push gorje6/ecs-nmap:latest
+
+docker build -t ecs-prowler .
+docker tag ecs-prowler:latest gorje6/ecs-prowler:latest
+docker push gorje6/ecs-prowler
---
+++ prowler/run_prowler.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Read the list of services from the environment variable
+# If not provided, use a default list (e.g., 's3 ec2'), or leave it empty to scan all services
+PROWLER_SERVICES=${PROWLER_SERVICES:-"s3 ec2"}
+
+# Run Prowler with verbose output
+prowler aws --services $PROWLER_SERVICES
+
+# Specify your S3 bucket name and desired folder (if any)
+BUCKET_NAME=${BUCKET_NAME:-"jorges-test"}
+FOLDER_NAME="prowler-reports"
+
+# Upload all files starting with "prowler-output-" to the S3 bucket
+aws s3 cp /usr/src/app/output/ "s3://$BUCKET_NAME/$FOLDER_NAME/" --recursive --exclude "*" --include "prowler-output-*"
+
+echo "Uploaded prowler output files to s3://$BUCKET_NAME/$FOLDER_NAME/"
+
+sleep infinity
