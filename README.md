ce6621b1c596a5618e634b3e8016609b565f4b08
jorgesoft <jorgecarrilloit@gmail.com>
2023-12-02 22:30:00 -0500

    created working pacu container with ssh

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ docker push gorje6/ecs-nmap:latest
 docker build -t ecs-prowler .
 docker tag ecs-prowler:latest gorje6/ecs-prowler:latest
 docker push gorje6/ecs-prowler
+
+docker build -t ecs-pacu .
+docker tag ecs-pacu:latest gorje6/ecs-pacu:latest
+docker push gorje6/ecs-pacu
---
+++ pacu/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Start SSH service
+service ssh start
+
+# Set the SSH password from environment variable
+echo "root:${SSH_PASSWD:-defaultpassword}" | chpasswd
+
+# Print the public IP address of the container
+echo "Fetching public IP..."
+curl -s http://checkip.amazonaws.com
+
+# Start SSH service
+/usr/sbin/sshd -D
+
+# Keep the container running
+tail -f /dev/null
